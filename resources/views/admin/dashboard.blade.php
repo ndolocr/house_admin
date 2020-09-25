9f1b988e0d5679b4a2ada364ead11f115903dabb
crndolo <ndoloraphael@gmail.com>
2020-09-25 19:31:45 +0300

    Creating dashboard page and including all styling sheets and javascript files

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,92 @@
+
+@extends('layouts.adminMaster')
+
+@section('title') Dashboard @endsection
+
+@section('page-title') Dashboard @endsection
+
+@section('dashboard-active') start active open @endsection
+
+@section('content')
+
+<!-- BEGIN DASHBOARD STATS 1-->
+<div class="row">
+	<!-- BEGIN COLUMN -->
+        <div class="col-md-12">
+
+            <!-- BEGIN PAGE TITLE-->
+            <h3 class="page-title"> Dashboard
+                <small>Statistics</small>
+            </h3>
+            <!-- END PAGE TITLE-->
+        </div>
+  
+
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="dashboard-stat blue">
+            <div class="visual">
+                <i class="fa fa-home"></i>
+            </div>
+            <div class="details">
+                <div class="number">
+                    <span data-counter="counterup" data-value="700">0</span>
+                </div>
+                <div class="desc"> Our Houses </div>
+            </div>
+            <a class="more" href=""> View more
+                <i class="m-icon-swapright m-icon-white"></i>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="dashboard-stat red">
+            <div class="visual">
+                <i class="fa fa-bath"></i>
+            </div>
+            <div class="details">
+                <div class="number">
+                    <span data-counter="counterup" data-value="200"></span> 
+                </div>
+                <div class="desc"> Our Rooms </div>
+            </div>
+            <a class="more" href=""> View more
+                <i class="m-icon-swapright m-icon-white"></i>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="dashboard-stat green">
+            <div class="visual">
+                <i class="fa fa-shopping-cart"></i>
+            </div>
+            <div class="details">
+                <div class="number">
+                    <span data-counter="counterup" data-value="1200"></span>
+                </div>
+                <div class="desc"> Tenants </div>
+            </div>
+            <a class="more" href=""> View more
+                <i class="m-icon-swapright m-icon-white"></i>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="dashboard-stat purple">
+            <div class="visual">
+                <i class="fa fa-globe"></i>
+            </div>
+            <div class="details">
+                <div class="number"> +
+                    <span data-counter="counterup" data-value="89"></span>% </div>
+                <div class="desc"> Transactions </div>
+            </div>
+            <a class="more" href="javascript:;"> View more
+                <i class="m-icon-swapright m-icon-white"></i>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="clearfix"></div>
+<!-- END DASHBOARD STATS 1-->    
+
+@endsection
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,28 +8,16 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>Muungano | User Login 1</title>
+        <title>Muungano | Login </title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta content="" name="description" />
         <meta content="" name="author" />
-        <!-- 
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
-        <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/pages/css/login-styles.min.css" rel="stylesheet" type="text/css" />
-        
-        <link rel="shortcut icon" href="favicon.ico" /> -->
+        <!-- BEGIN STYLES -->
         <link href="{{ asset('bootstrap/css/bootstrap.css') }}" rel="stylesheet">
         <link href="{{ asset('css/components.css') }}" rel="stylesheet">
         <link href="{{ asset('css/login-styles.css') }}" rel="stylesheet">
+        <!-- END STYLES -->
     </head>
     <!-- END HEAD -->
 
