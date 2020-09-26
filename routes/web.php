0cf8ba21f5eab1e041ca5501949d0c797b091f05
crndolo <ndoloraphael@gmail.com>
2020-09-26 23:00:19 +0300

    Creating route for deleting house record

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-//Route::get('/house/create', 'HouseController@create')->name('create-house');
-
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+
 Route::get('/house', [App\Http\Controllers\HouseController::class, 'index'])->name('all-houses');
 Route::post('/house/store', [App\Http\Controllers\HouseController::class, 'store'])->name('store-house');
 Route::get('/house/edit/{id}', [App\Http\Controllers\HouseController::class, 'edit'])->name('edit-house');
 Route::get('/house/create', [App\Http\Controllers\HouseController::class, 'create'])->name('create-house');
 Route::put('/house/edit/{id}', [App\Http\Controllers\HouseController::class, 'update'])->name('update-house');
+Route::get('/house/delete/{id}' [App\Http\Controllers\HouseController::class, 'delete'])->name('delete-house');
 
 
