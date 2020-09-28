ec3fb8c9816aad3c5e36ec7a9d4f72811d91687a
crndolo <ndoloraphael@gmail.com>
2020-09-28 09:08:01 +0300

    Creating room function in Tenant model to link a many to many relationship between tenant and room

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 use App\Models\House;
+use App\Models\Tenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Room;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tenant extends Model
 {
     use HasFactory;
+
+    public function room(){
+    	return $this->belongsToMany(Room::class);
+    }
 }
